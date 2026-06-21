cb68cd1ad2d72f41f74955746f86ba1b0d34877f
WanguiThiongo <wangoithiongo@gmail.com>
2026-06-21 21:50:47 +0300

    Clean App.js no Footer

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Signin from './components/Signin';
 import Addproducts from './components/Addproducts';
 import Makepayment from './components/Makepayment';
 import Notfound from './components/Notfound';
-import Footer from './components/Footer';
 import "bootstrap/dist/css/bootstrap.min.css"; 
 import "bootstrap/dist/js/bootstrap.min.js"
 
@@ -14,7 +13,6 @@ function App() {
   return (
     <Router>
       <div className="App">
-
         <Routes>
           <Route path='/' element={<Getproducts/>}/>
           <Route path='/signup' element={<Signup/>}/>
@@ -23,7 +21,6 @@ function App() {
           <Route path='/makepayment' element={<Makepayment/>}/>
           <Route path='*' element={<Notfound/>}/>
         </Routes>
-
       </div>
     </Router>
   )
