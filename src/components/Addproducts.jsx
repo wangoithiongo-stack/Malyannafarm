2bd7f8b8939f488a99bc0eab7f93739fade25aaa
WanguiThiongo <wangoithiongo@gmail.com>
2026-06-22 18:23:19 +0300

    Add navbar and footer to add products

--- src/components/Addproducts.jsx
+++ src/components/Addproducts.jsx
@@ -1,31 +1,34 @@
 // import axios from 'axios'
 import axios from 'axios'
 import React, { useState } from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
+
 
 const Addproducts = () => {
-// Step 1 : Declare hooks
-const [product_name, setProductname] = useState("") 
-const [product_description, setProductdescription] = useState("")
-const [product_cost, setProductcost] = useState("")
-const [product_photo, setProductphoto] =useState("")
+  // Step 1 : Declare hooks
+  const [product_name, setProductname] = useState("")
+  const [product_description, setProductdescription] = useState("")
+  const [product_cost, setProductcost] = useState("")
+  const [product_photo, setProductphoto] = useState("")
 
-// Step 3 : Add three additional hooks
-const [loading,setLoading] = useState("")
-const [success,setSuccess] = useState("")
-const [error,setError] = useState("")
+  // Step 3 : Add three additional hooks
+  const [loading, setLoading] = useState("")
+  const [success, setSuccess] = useState("")
+  const [error, setError] = useState("")
 
-// Step 4 : Function to handle the submit button
-const handlesubmit = async (e) => {
+  // Step 4 : Function to handle the submit button
+  const handlesubmit = async (e) => {
 
-   // Step 5 : Prevent defaults - This means you are preventing the browser from reloading when a user submits details for registraion
+    // Step 5 : Prevent defaults - This means you are preventing the browser from reloading when a user submits details for registraion
     e.preventDefault()
 
     // Step 6 : Update loading hook
     setLoading("Product waiting to be added...")
 
     // Step 7 : Add try and catch block
-    try{
-       const formdata = new FormData()
+    try {
+      const formdata = new FormData()
 
       //  Step 9 : Append
       formdata.append("product_name", product_name)
@@ -47,71 +50,76 @@ const handlesubmit = async (e) => {
       setProductphoto("")
     }
 
-    catch(error){
+    catch (error) {
       // Step 13 : 
-       setLoading("")
-       setError("Product not added. Retry!")
+      setLoading("")
+      setError("Product not added. Retry!")
     }
 
-}
+  }
 
   return (
-    <div className='row justify-content-center mt-5'>
+    <>
+      <Navbar />
+      <div className='row justify-content-center mt-5'>
 
-      <div className='col-md-6 p-4 card shadow'>
-        <h1>Add Product</h1>
+        <div className='col-md-6 p-4 card shadow'>
+          <h1>Add Product</h1>
           <form onSubmit={handlesubmit}>
 
             <h4 className='text-info'>{loading}</h4>
             <h4 className='text-success'>{success}</h4>
             <h4 className='text-danger'>{error}</h4>
 
-          {/* Step 2 : Test the hooks */}
-          <input type="text"
-          value={product_name}
-          onChange={(e) => setProductname(e.target.value)}
-          placeholder='Enter product name'
-          className='form-control'
-          required />
-          <br />
+            {/* Step 2 : Test the hooks */}
+            <input type="text"
+              value={product_name}
+              onChange={(e) => setProductname(e.target.value)}
+              placeholder='Enter product name'
+              className='form-control'
+              required />
+            <br />
 
-          {/* {product_name} */}
+            {/* {product_name} */}
 
-          <input type="text"
-          value={product_description}
-          onChange={(e) => setProductdescription(e.target.value)}
-          placeholder='Enter product description'
-          className='form-control'
-          required />
-          <br />
+            <input type="text"
+              value={product_description}
+              onChange={(e) => setProductdescription(e.target.value)}
+              placeholder='Enter product description'
+              className='form-control'
+              required />
+            <br />
 
-          {/* {product_description} */}
+            {/* {product_description} */}
 
-          <input type="number"
-          value={product_cost}
-          onChange={(e) => setProductcost(e.target.value)}
-          placeholder='Enter the price of your product' 
-          className='form-control'
-          required/>
-          <br />
+            <input type="number"
+              value={product_cost}
+              onChange={(e) => setProductcost(e.target.value)}
+              placeholder='Enter the price of your product'
+              className='form-control'
+              required />
+            <br />
 
-           {/* {product_cost} */}
+            {/* {product_cost} */}
 
-          <label>Product photo</label>
-          <br />
-          <input type="file" 
-          accept='image/*'
-          onChange={(e) => setProductphoto(e.target.files[0])}
-          className='form-control' />
-          <br />
+            <label>Product photo</label>
+            <br />
+            <input type="file"
+              accept='image/*'
+              onChange={(e) => setProductphoto(e.target.files[0])}
+              className='form-control' />
+            <br />
 
-          <input type="submit"
-          value="Add product" 
-          className='btn btn-success'/>
-        </form>
+            <input type="submit"
+              value="Add product"
+              className='btn btn-success' />
+          </form>
+        </div>
+
       </div>
 
-    </div>
+      <Footer />
+    </>
   )
 }
 
